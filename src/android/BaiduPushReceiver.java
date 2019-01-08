702a33c6df242af55d7b8e7a0a9c8408ae655a1b
jessica88888 <jessicachan365@gmail.com>
2019-01-09 00:13:31 +0800

    Update BaiduPushReceiver.java

--- src/android/BaiduPushReceiver.java
+++ src/android/BaiduPushReceiver.java
@@ -18,6 +18,10 @@ import org.apache.cordova.CallbackContext;
 
 import com.baidu.android.pushservice.PushMessageReceiver;
 
+/**custom start*/
+public static final String FOREGROUND = "foreground";
+public static final String START_IN_BACKGROUND = "cdvStartInBackground";
+/**custom end*/
 /*
  * Push消息处理receiver。请编写您需要的回调函数， 一般来说： onBind是必须的，用来处理startWork返回值；
  *onMessage用来接收透传消息； onSetTags、onDelTags、onListTags是tag相关操作的回调；
