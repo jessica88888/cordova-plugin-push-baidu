1b01bf6975e9b877ec534b8ec9ea1df0720dd44d
jessica88888 <jessicachan365@gmail.com>
2019-01-10 23:58:03 +0800

    Update BaiduPushReceiver.java

--- src/android/BaiduPushReceiver.java
+++ src/android/BaiduPushReceiver.java
@@ -254,6 +254,8 @@ public class BaiduPushReceiver extends PushMessageReceiver {
                 intent.putExtra("foreground", false);
                 context.startActivity(intent);
                 Log.d(TAG, "test123");*/
+                
+                
                 /*custom end*/
 
 
@@ -264,39 +266,19 @@ public class BaiduPushReceiver extends PushMessageReceiver {
                 Log.d(TAG, jsonObject.toString());
                 
                 
-                NotificationCompat.Builder mBuilder =
-                    new NotificationCompat.Builder(this)
-                    .setSmallIcon(R.drawable.notification_icon)
-                    .setContentTitle("My notification")
-                    .setContentText("Hello World!");
+                NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, "abcd")
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle("title")
+                .setContentText("text")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                // notificationId is a unique int for each notification that you must define
+                notificationManager.notify(notificationId, mBuilder.build());
 
 
-                // Gets an instance of the NotificationManager service//
-
-                NotificationManager mNotificationManager =
-
-                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-                // When you issue multiple notifications about the same type of event,
-                // it’s best practice for your app to try to update an existing notification
-                // with this new information, rather than immediately creating a new notification.
-                // If you want to update this notification at a later date, you need to assign it an ID.
-                // You can then use this ID whenever you issue a subsequent notification.
-                // If the previous notification is still visible, the system will update this existing notification,
-                // rather than create a new one. In this example, the notification’s ID is 001//
-
-                NotificationManager.notify().
-
-                mNotificationManager.notify(001, mBuilder.build());
 
 
-                
-                
-                
-                
-                
-                
-                
+
                 
                 
                 
