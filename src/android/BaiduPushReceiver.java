920a9d7154815eb0df372b8c4b4181a11db17639
jessica88888 <jessicachan365@gmail.com>
2019-01-12 00:19:43 +0800

    Update BaiduPushReceiver.java

--- src/android/BaiduPushReceiver.java
+++ src/android/BaiduPushReceiver.java
@@ -265,7 +265,26 @@ public class BaiduPushReceiver extends PushMessageReceiver {
                 .setContentText("textcontent")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
-                Notification notification = mBuilder.getNotification();
+               // Notification notification = mBuilder.getNotification();
+                
+                
+                 // Gets an instance of the NotificationManager service//
+
+        NotificationManager mNotificationManager =
+
+            (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // When you issue multiple notifications about the same type of event,
+        // it’s best practice for your app to try to update an existing notification
+        // with this new information, rather than immediately creating a new notification.
+        // If you want to update this notification at a later date, you need to assign it an ID.
+        // You can then use this ID whenever you issue a subsequent notification.
+        // If the previous notification is still visible, the system will update this existing notification,
+        // rather than create a new one. In this example, the notification’s ID is 001//
+
+        NotificationManager.notify().
+
+        mNotificationManager.notify(001, mBuilder.build());
                 
                 /*
                 
